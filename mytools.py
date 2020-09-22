3357abca402c34ad099b4746b692b529ee39caf3
Zahidul Hoque <zahidul23@gmail.com>
2020-09-21 20:12:57 -0400

    first commit

---
+++ mytools.py
@@ -0,0 +1,213 @@
+import time
+import os
+import json
+from ffmpy import FFmpeg
+from requests_toolbelt import MultipartEncoder, MultipartEncoderMonitor
+import requests
+from PyQt5.QtCore import pyqtSignal,QThread
+import threading
+
+
+def verifyStreamableAuth(username,password):
+	r = requests.post('https://api.streamable.com/upload',auth=(username, password))
+
+	if r is None:
+		return False
+	if r.status_code != 400:
+		return False
+	return True
+
+class FileUploader(QThread):
+	upload_progress = pyqtSignal(object)
+	upload_complete = pyqtSignal(object)
+	processing_complete = pyqtSignal(object)
+	def __init__(self,filePath, username, password):
+		QThread.__init__(self)
+		self.filePath = filePath
+		self.fileName = os.path.split(filePath)[1]
+		self.username = username
+		self.password = password
+
+	def __del__(self):
+		return
+		self.wait()
+
+	def run(self):
+		self.upload()
+
+	def my_callback(self,monitor):
+		# Your callback function
+		progress = "%d%%" %(monitor.bytes_read/monitor.len * 100)
+		self.upload_progress.emit(progress)
+		#print("%d%%" %(monitor.bytes_read/monitor.len * 100))
+
+	def upload(self,):
+		e = MultipartEncoder(
+		fields={'field0': 'value', 'field1': 'value',
+				'field2': (self.fileName, open(self.filePath, 'rb'), 'text/plain')}
+		)
+		m = MultipartEncoderMonitor(e, self.my_callback)
+
+		r = requests.post('https://api.streamable.com/upload',auth=(self.username, self.password), data=m, headers={'Content-Type': m.content_type})
+
+		if r.status_code == 200:
+			self.upload_complete.emit(r.json())
+			
+			self.checkProcessing(r.json()['shortcode'])
+
+		else:
+			self.upload_complete.emit('Upload Failed')
+
+	def checkProcessing(self,sc):
+		p_stat = 1
+		url = 'https://api.streamable.com/videos/' + sc
+		stime =  time.time()
+		toTime =  stime + 600
+		waitTime = 2
+		while p_stat == 1:
+			if time.time() > toTime:
+				self.processing_complete.emit("time-out")
+				return
+			r = requests.get(url)
+			p_stat =  r.json()['status']
+			waitTime = self.getWaitTime(waitTime)
+
+		vidurl = 'https://streamable.com/' + sc
+		self.processing_complete.emit(vidurl)
+
+	def getWaitTime(self,sec):
+		time.sleep(sec)
+		if sec < 60:
+			sec = sec * 2
+		return sec
+
+
+def uploadToStreamable(filePath, username, password):
+	def my_callback(monitor):
+		# Your callback function
+		print("%d%%" %(monitor.bytes_read/monitor.len * 100))
+		#if (monitor.bytes_read/8192) % 5 == 0:
+			#time.sleep(1/25)
+
+	e = MultipartEncoder(
+		fields={'field0': 'value', 'field1': 'value',
+				'field2': ('filename', open(filePath, 'rb'), 'text/plain')}
+		)
+	m = MultipartEncoderMonitor(e, my_callback)
+
+	r = requests.post('https://api.streamable.com/upload',auth=(username, password), data=m,
+					  headers={'Content-Type': m.content_type})
+				  
+	print(r.status_code)
+	print(r.json())	
+
+
+def startUploadThread(filePath, username, password):
+	uT = threading.Thread(target=startUploadThread, args=(filePath, username, password))
+	uT.start()
+
+def getAvailableName(mypath, add):
+	noExt, ext = os.path.splitext(mypath)
+	newName =  noExt + add + ext
+	addNum = 1
+
+	while(os.path.exists(newName)):
+		newName =  "%s%s(%d)%s" % (noExt,add,addNum,ext)
+		addNum += 1
+
+	return newName
+
+
+
+def randName():
+	integer = time.time_ns()
+	chars = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+	result = ''
+	while integer > 0:
+		integer, remainder = divmod(integer, 36)
+		result = chars[remainder]+result
+	return result
+
+def fast_scandir(dirname):
+	subfolders= [f.path for f in os.scandir(dirname) if f.is_dir()]
+	for dirname in list(subfolders):
+		subfolders.extend(fast_scandir(dirname))
+	return subfolders
+
+def getFiles(mypath):
+	files = []
+	for dirpath, dirnames, filenames in os.walk(mypath):
+		for filename in [f for f in filenames if f.endswith(('.mp4','.mkv','.flv','.gif',))]:
+			fullPath = os.path.join(dirpath, filename)
+			files.append((fullPath, filename))
+	files.sort(key= lambda tup: os.path.getctime(tup[0]), reverse=False)		
+	return files
+
+def getThumbs():
+	thumbs = {}
+	if os.path.exists('thumbs.json'):
+		with open('thumbs.json') as json_file:
+			thumbs = json.load(json_file)
+	return thumbs
+
+def getThumb(filepath):
+	thumbs = getThumbs()
+	thumbName = ''
+	if filepath not in thumbs:
+		thumbName = randName()
+		thumbName = 'thumbs/'+ thumbName+ '.png'
+		try:
+			ff = FFmpeg(inputs={filepath: None}, outputs={thumbName: ['-ss', '00:00:00.000', '-vframes', '1','-s', '256x144']})
+			ff.run()
+		except:
+			None
+
+	thumbs[filepath] = thumbName
+	with open('thumbs.json', 'w') as outfile:
+		json.dump(thumbs, outfile)
+
+
+	
+	return thumbName
+
+def checkThumbs(filepaths):
+	thumbs = getThumbs()
+
+	for mypath in filepaths:
+		if mypath not in thumbs:
+			thumbName = randName()
+			thumbName = 'thumbs/'+ thumbName+ '.png'
+			try:
+				ff = FFmpeg(inputs={mypath: None}, outputs={thumbName: ['-ss', '00:00:00.000', '-vframes', '1','-s', '256x144']})
+				ff.run()
+				thumbs[mypath] = thumbName
+			except:
+				None
+
+	with open('thumbs.json', 'w') as outfile:
+		json.dump(thumbs, outfile)
+
+def getFilesCheckingThumbs(mypath):
+	files = []
+	thumbs = getThumbs()
+
+
+
+	for dirpath, dirnames, filenames in os.walk(mypath):
+		for filename in [f for f in filenames if f.endswith(('.mp4','.mkv','.flv','.gif',))]:
+			fullPath = os.path.join(dirpath, filename)
+			files.append((fullPath, filename))
+			if fullPath not in thumbs:
+				thumbName = randName()
+				thumbName = 'thumbs/'+ thumbName+ '.png'
+				ff = FFmpeg(inputs={fullPath: None}, outputs={thumbName: ['-ss', '00:00:1', '-vframes', '1','-s', '256x144']})
+				ff.run()
+				thumbs[fullPath] = thumbName
+
+	with open('thumbs.json', 'w') as outfile:
+		json.dump(thumbs, outfile)
+
+	files.sort(key= lambda tup: os.path.getctime(tup[0]), reverse=False)
+
+	return files
+
---
+++ run.py
@@ -0,0 +1,559 @@
+import sys
+from PyQt5.QtWidgets import QDialog, QFileDialog, QMenu, QTimeEdit, QAction, QLabel,QScrollArea
+from PyQt5.QtWidgets import QApplication,QPushButton,QLineEdit, QMessageBox,QStyle
+from PyQt5.QtWidgets import QWidget,QMainWindow,QGridLayout,QVBoxLayout,QHBoxLayout, QSizePolicy, QSlider 
+from PyQt5.QtGui import QPalette, QColor, QIcon, QCursor
+from PyQt5.QtMultimedia import QMediaContent, QMediaPlayer
+from PyQt5.QtMultimediaWidgets import QVideoWidget
+from PyQt5 import QtCore, QtTest
+from watchdog.observers import Observer
+from watchdog.events import PatternMatchingEventHandler
+import mytools
+import qrangeslider #Source: https://stackoverflow.com/a/47342469 ,https://github.com/rsgalloway/qrangeslider
+import time
+import json
+import subprocess
+import os
+from ffmpy import FFmpeg
+import webbrowser
+
+
+
+class UploadThread(QtCore.QThread):
+	def __init__(self,filePath,username,password):
+		QtCore.QThread.__init__(self)
+		self.user = username
+		self.pw = password
+		self.fPath =  filePath
+
+	def __del__(self):
+		self.wait()
+
+	def run(self):
+		mytools.uploadToStreamable(self.fPath, self.user,self.pw)
+
+
+class VideoWindow(QWidget):
+	def __init__(self, vidPath):
+		super().__init__()
+		self.fullPath = vidPath
+		self.startTime = 0
+		self.endTime = 0
+		self.init_ui()
+
+
+	def init_ui(self):
+		layout = QVBoxLayout()
+		self.setLayout(layout)
+		self.setWindowTitle(self.fullPath)
+
+		self.mediaPlayer = QMediaPlayer(None, QMediaPlayer.VideoSurface)
+
+		videoWidget = QVideoWidget()
+
+		self.playButton = QPushButton()
+		self.playButton.setEnabled(True)
+		self.playButton.setIcon(self.style().standardIcon(QStyle.SP_MediaPlay))
+		self.playButton.setFixedWidth(100)
+		self.playButton.setFixedHeight(50)
+		self.playButton.clicked.connect(self.play)
+
+		self.trimButton = QPushButton("Trim")
+		self.trimButton.setFixedWidth(150)
+		self.trimButton.setFixedHeight(50)
+		self.trimButton.clicked.connect(self.trimVid)
+
+
+		self.positionSlider = QSlider(QtCore.Qt.Horizontal)
+		self.positionSlider.setRange(0, 0)
+		self.positionSlider.sliderMoved.connect(self.setPosition)
+
+		self.rangeSlider = qrangeslider.QRangeSlider()
+		self.rangeSlider.setRange(0, 0)
+		self.rangeSlider.endValueChanged.connect(self.adjustForEnd)
+		self.rangeSlider.startValueChanged.connect(self.adjustForStart)
+		self.rangeSlider.setFixedHeight(15)
+
+
+		self.startTimeInput = QTimeEdit()
+		self.endTimeInput = QTimeEdit()
+		self.startTimeInput.setDisplayFormat('hh:mm:ss.zzz')
+		self.endTimeInput.setDisplayFormat('hh:mm:ss.zzz')
+
+		self.startTimeInput.timeChanged.connect(self.startInputChanged)
+		self.endTimeInput.timeChanged.connect(self.endInputChanged)
+
+		self.mediaPlayer.setMedia(QMediaContent(QtCore.QUrl.fromLocalFile(self.fullPath)))
+
+
+		layout.addWidget(videoWidget)
+		self.mediaPlayer.setVideoOutput(videoWidget)
+		self.mediaPlayer.setNotifyInterval(10)
+		self.mediaPlayer.stateChanged.connect(self.mediaStateChanged)
+		self.mediaPlayer.positionChanged.connect(self.positionChanged)
+		self.mediaPlayer.durationChanged.connect(self.durationChanged)
+
+		
+
+		controlLayout = QVBoxLayout()
+		controlLayout.setContentsMargins(0, 0, 0, 0)
+		controlLayout.addWidget(self.rangeSlider)
+		controlLayout.addWidget(self.positionSlider)
+
+		timeInputLayout = QHBoxLayout()
+		timeInputLayout.addWidget(self.playButton)
+		timeInputLayout.addWidget(self.startTimeInput)
+		timeInputLayout.addWidget(self.endTimeInput)
+		timeInputLayout.addWidget(self.trimButton)
+
+		controlLayout.addLayout(timeInputLayout)
+
+
+		layout.addLayout(controlLayout)
+
+		self.mediaPlayer.play()
+
+		self.resize(1024,700)
+
+		self.show()
+
+
+	def closeEvent(self, event):
+		self.mediaPlayer.stop()
+
+	def trimVid(self):
+		self.trimButton.setEnabled(False)
+		outName = mytools.getAvailableName(self.fullPath,'Trim')
+		print(outName)
+		trimStartTime = self.startTimeInput.time().toString('hh:mm:ss.zzz')
+		trimEndTime = self.endTimeInput.time().toString('hh:mm:ss.zzz')
+		try:
+			ff = FFmpeg(inputs={self.fullPath: None}, outputs={outName: ['-ss', trimStartTime, '-to', trimEndTime,'-c:v', 'copy','-c:a','copy',]})
+			ff.run()
+		except Exception as e:
+			msg = QMessageBox()
+			msg.setWindowTitle("Trim Failed")
+			msg.setText(str(e))
+			msg.setIcon(QMessageBox.Critical)
+
+			showMsg = msg.exec_()
+		self.trimButton.setEnabled(True)
+
+
+
+	def play(self):
+		if self.mediaPlayer.state() == QMediaPlayer.PlayingState:
+			self.mediaPlayer.pause()
+		else:
+			self.mediaPlayer.play()
+
+	def mediaStateChanged(self, state):
+		if self.mediaPlayer.state() == QMediaPlayer.PlayingState:
+			self.playButton.setIcon(
+				self.style().standardIcon(QStyle.SP_MediaPause))
+		else:
+			self.playButton.setIcon(
+				self.style().standardIcon(QStyle.SP_MediaPlay))
+
+	def positionChanged(self, position):
+		self.positionSlider.setValue(position)
+		if position > self.endTime:
+			self.mediaPlayer.setPosition(self.startTime)
+
+	def adjustForStart(self, startPos):
+		self.startTime =  startPos
+		if self.positionSlider.value() < startPos:
+			self.mediaPlayer.setPosition(startPos)
+
+		self.startTimeInput.setTime(QtCore.QTime(0,0).addMSecs(startPos))
+		self.endTimeInput.setMinimumTime(QtCore.QTime(0,0).addMSecs(startPos))
+
+	def adjustForEnd(self, endPos):
+		self.endTime = endPos
+		if self.positionSlider.value() > endPos:
+			self.mediaPlayer.setPosition(endPos)
+
+		self.endTimeInput.setTime(QtCore.QTime(0,0).addMSecs(endPos))
+		self.startTimeInput.setMaximumTime(QtCore.QTime(0,0).addMSecs(endPos))
+
+	def startInputChanged(self, inputTime):
+		self.rangeSlider.setStart(QtCore.QTime(0,0,0,0).msecsTo(inputTime))
+
+	def endInputChanged(self, inputTime):
+		self.rangeSlider.setEnd(QtCore.QTime(0,0,0,0).msecsTo(inputTime))
+		
+	def durationChanged(self, duration):
+		self.positionSlider.setRange(0, duration)
+		self.rangeSlider.setMax(duration)
+		self.rangeSlider.setEnd(duration)
+
+		self.startTimeInput.setMinimumTime(QtCore.QTime(0,0))
+		self.endTimeInput.setMinimumTime(QtCore.QTime(0,0))
+		self.endTimeInput.setTime(QtCore.QTime(0,0).addMSecs(duration))
+		self.startTimeInput.setMaximumTime(QtCore.QTime(0,0).addMSecs(duration))
+		self.endTimeInput.setMaximumTime(QtCore.QTime(0,0).addMSecs(duration))
+
+	def setPosition(self, position):
+		self.mediaPlayer.setPosition(position)
+
+class SettingsWindow(QDialog):
+	def __init__(self):
+		super().__init__()
+		self.settings = {}
+		self.username = ''
+		self.password = ''
+		self.rootpath = ''
+		self.init_ui()
+
+	def init_ui(self):
+		layout = QVBoxLayout()
+		self.setLayout(layout)
+
+		if os.path.exists('settings.json'):
+			with open('settings.json') as json_file:
+				self.settings = json.load(json_file)
+
+
+		fpLabel =  QLabel("Videos Folder: ")
+
+		fdHBox = QHBoxLayout()
+		self.fdInput = QLineEdit()
+		self.fdInput.setReadOnly(True)
+		fdButton = QPushButton("Browse")
+		fdButton.clicked.connect(self.setRootFolder)
+		fdHBox.addWidget(self.fdInput)
+		fdHBox.addWidget(fdButton)
+
+		uHBox = QHBoxLayout()
+		userLabel = QLabel("Username: ")
+		self.userInput = QLineEdit()
+		uHBox.addWidget(userLabel)
+		uHBox.addWidget(self.userInput)
+
+		pHBox = QHBoxLayout()
+		pwLabel = QLabel("Password: ")
+		self.pwInput = QLineEdit()
+		self.pwInput.setEchoMode(QLineEdit.Password)
+		pHBox.addWidget(pwLabel)
+		pHBox.addWidget(self.pwInput)
+
+
+		saveButton =  QPushButton("Save")
+		saveButton.clicked.connect(self.saveSettings)
+		saveButton.setDefault(True)
+
+		layout.addWidget(fpLabel)
+		layout.addLayout(fdHBox)
+		layout.addLayout(uHBox)
+		layout.addLayout(pHBox)
+		layout.addWidget(saveButton)
+
+
+		self.setWindowTitle("Settings")
+		self.resize(640,480)
+		#self.show()
+
+	def setRootFolder(self):
+		file = str(QFileDialog.getExistingDirectory(self, "Select Directory"))
+		self.fdInput.setText(file)
+	def saveSettings(self):
+		self.settings['username'] = self.userInput.text()
+		self.settings['password'] = self.pwInput.text()
+		self.settings['root_path'] = self.fdInput.text()
+		with open('settings.json', 'w') as outfile:
+			json.dump(self.settings, outfile)
+
+		self.close()
+	def getSettings(self):
+		return(self.settings)
+
+class FileObserver(QtCore.QObject):
+	file_found = QtCore.pyqtSignal(object)
+	def __init__(self, rootPath):
+		super().__init__()
+		self.rootPath = rootPath
+		self.patterns = ["*.mkv", "*.mp4", "*.flv", "*.gif"]
+		self.ignore_patterns = []
+		self.ignore_directories = True
+		self.case_sensitive = False
+		self.run()
+	
+	def on_created(self, event):
+		print(f"{event.src_path} has been created")
+		QtTest.QTest.qWait(3000)
+		self.file_found.emit(event.src_path)
+
+	def on_deleted(self, event):
+		print(f"{event.src_path} has been deleted")
+
+	def on_modified(self, event):
+		print(f"{event.src_path} has been modified")
+
+	def on_moved(self, event):
+		print(f"{event.src_path} has moved to {event.dest_path}")
+
+	def run(self):
+		my_event_handler = PatternMatchingEventHandler(self.patterns, self.ignore_patterns, self.ignore_directories, self.case_sensitive)
+	
+		my_event_handler.on_created = self.on_created
+		my_observer = Observer()
+		my_observer.schedule(my_event_handler, self.rootPath, recursive=True)
+		my_observer.start()
+
+
+class MainWindow(QMainWindow):
+	def __init__(self):
+		super().__init__()
+		self.gridx = 3
+		self.gridy = 999999
+		self.mypath = ""
+		self.vw = None
+		self.username = ''
+		self.password = ''
+		self.vidButtons = []
+		self.vidLabels = []
+		self.vidPaths = []
+		self.numVids = 0
+		self.init_ui()
+
+	def init_ui(self):
+
+		self.scroll = QScrollArea()
+		self.widget = QWidget()
+		self.gridLayout = QGridLayout()
+
+		self.widget.setLayout(self.gridLayout)
+		self.popMenu = QMenu(self)
+		self.cursor = QCursor()
+
+		try:
+			os.makedirs('thumbs')
+		except OSError as e:
+			if e.errno != errno.EEXIST:
+				raise
+
+		self.loadSettings()
+		self.loadGrid()
+
+		self.scroll.setVerticalScrollBarPolicy(QtCore.Qt.ScrollBarAlwaysOn)
+		self.scroll.setHorizontalScrollBarPolicy(QtCore.Qt.ScrollBarAlwaysOff)
+		self.scroll.setWidgetResizable(True)
+		self.scroll.setWidget(self.widget)
+
+		fObs = FileObserver(self.mypath)
+		fObs.file_found.connect(self.addVid)
+
+
+		self.setCentralWidget(self.scroll)
+		self.setWindowTitle("Clips")
+		self.setGeometry(520,260,1030,590)
+		self.show()
+
+
+	def loadSettings(self):
+		settings = {}
+
+		if os.path.exists('settings.json'):
+			with open('settings.json') as json_file:
+				settings = json.load(json_file)
+
+		if 'root_path' not in settings:
+			self.sw = SettingsWindow()
+			self.sw.exec_()
+			settings = self.sw.getSettings()
+		elif not os.path.exists(settings['root_path']):
+			self.sw = SettingsWindow()
+			self.sw.exec_()
+			settings = self.sw.getSettings()
+
+		if 'root_path' not in settings:
+			sys.exit()
+		if not os.path.exists(settings['root_path']):
+				sys.exit()
+
+		self.username =  settings['username']
+		self.password =  settings['password']
+		self.mypath =  settings['root_path']
+
+	def addVid(self, newVidPath):
+		fl = [newVidPath, os.split(newVidPath)[1]] 
+		x = self.gridx
+		y = self.gridy
+		if x==0:
+			x=3
+			y-=2
+
+		n = self.numVids
+		self.vidButtons.append(QPushButton())
+
+		self.vidButtons[n].setContextMenuPolicy(QtCore.Qt.CustomContextMenu)
+		self.vidButtons[n].customContextMenuRequested.connect(lambda state, vPath = fl: self.on_buttonRightClick(vPath))
+
+
+
+		self.vidButtons[n].setIcon(QIcon(mytools.getThumb(newVidPath)))
+		self.vidButtons[n].setIconSize(QtCore.QSize(256,144))
+		self.vidButtons[n].clicked.connect(lambda state, vPath = fl[0]: self.openVideoWindow(self,vPath))
+		try:
+			self.gridLayout.itemAtPosition(y,x).widget().setParent(None)
+		except:
+			pass
+		self.gridLayout.addWidget(self.vidButtons[n],y,x)
+
+		self.vidLabels.append(QLabel(fl[1]))
+		self.vidLabels[n].setMaximumWidth(300)
+		self.vidLabels[n].setAlignment(QtCore.Qt.AlignCenter)
+		try:
+			self.gridLayout.itemAtPosition(y+1,x).widget().setParent(None)
+		except:
+			pass
+		self.gridLayout.addWidget(self.vidLabels[n],y+1,x)
+
+		self.numVids += 1
+		x-=1
+		self.x = x		
+
+
+	def openVideoWindow(self,checked,vidPath):
+		self.vw = VideoWindow(vidPath)
+
+
+	def on_buttonRightClick(self, fileData):
+		self.popMenu = QMenu(self)
+
+		folderPath = os.path.split(fileData[0])[0]
+
+		openFolderAction = QAction('Open Folder', self)
+		openFolderAction.triggered.connect(lambda state, dPath =fileData[0]: self.openFolderExplorer(dPath))
+
+
+		uploadToStreamable = QAction('Upload', self)
+		uploadToStreamable.triggered.connect(lambda state, fPath =fileData[0]: self.uploadStreamable(fPath))
+
+		self.popMenu.addAction(openFolderAction)
+		self.popMenu.addSeparator()
+		self.popMenu.addAction(uploadToStreamable) 
+
+		self.popMenu.popup(self.cursor.pos())
+
+	def showUploadProgress(self, progress):
+		print(progress)
+
+	def onUploadComplete(self, response):
+		print(response)
+	def onProcessingComplete(self, vidurl):
+		print(vidurl)
+		webbrowser.open(vidurl, new=2)
+
+	def uploadStreamable(self, folderPath):
+		self.fileUp = mytools.FileUploader(folderPath, self.username,self.password)
+		self.fileUp.start()
+		self.fileUp.upload_progress.connect(self.showUploadProgress)
+		self.fileUp.upload_complete.connect(self.onUploadComplete)
+		self.fileUp.processing_complete.connect(self.onProcessingComplete)
+
+	def openFolderExplorer(self, folderPath):
+		#print(folderPath)
+		folderPath = folderPath.replace('/','\\')
+		subprocess.Popen(r'explorer /select,%s'%folderPath)
+
+	'''
+	def rereloadGrid(self):
+		self.reloadGrid()
+
+	def reloadGGGrid(self):
+		self.gridx = (self.numVids%3)
+		self.gridy = 999999
+		for i in range(self.numVids):
+			print(i)
+			if self.gridx==0:
+				self.gridx=3
+				self.gridy-=2
+			try:
+				self.gridLayout.itemAtPosition(self.gridy,self.gridx).widget().setParent(None)
+			finally:
+				self.gridLayout.addWidget(self.vidButtons[i], self.gridy,self.gridx)
+			try:
+				self.gridLayout.itemAtPosition(self.gridy+1,self.gridx).widget().setParent(None)
+			finally:
+				self.gridLayout.addWidget(self.vidLabels[i], self.gridy+1,self.gridx)
+			self.gridx -=1
+	'''
+	def loadGrid(self):
+		self.paths = mytools.getFilesCheckingThumbs(self.mypath)
+		x=len(self.paths)%3
+		y=999999
+
+		thumbs = mytools.getThumbs()
+
+		for fl in self.paths:
+			if x==0:
+				x=3
+				y-=2
+
+			n = self.numVids
+			self.vidButtons.append(QPushButton())
+
+			self.vidButtons[n].setContextMenuPolicy(QtCore.Qt.CustomContextMenu)
+			self.vidButtons[n].customContextMenuRequested.connect(lambda state, vPath = fl: self.on_buttonRightClick(vPath))
+
+
+
+			self.vidButtons[n].setIcon(QIcon(thumbs[fl[0]]))
+			self.vidButtons[n].setIconSize(QtCore.QSize(256,144))
+			self.vidButtons[n].clicked.connect(lambda state, vPath = fl[0]: self.openVideoWindow(self,vPath))
+			try:
+				self.gridLayout.itemAtPosition(y,x).widget().setParent(None)
+			except:
+				pass
+			self.gridLayout.addWidget(self.vidButtons[n],y,x)
+
+			self.vidLabels.append(QLabel(fl[1]))
+			self.vidLabels[n].setMaximumWidth(300)
+			self.vidLabels[n].setAlignment(QtCore.Qt.AlignCenter)
+			try:
+				self.gridLayout.itemAtPosition(y+1,x).widget().setParent(None)
+			except:
+				pass
+			self.gridLayout.addWidget(self.vidLabels[n],y+1,x)
+
+			self.numVids += 1
+			x-=1			
+
+		self.gridx = x
+		self.gridy = y
+
+
+
+def directory_changed(path):
+	print('Directory Changed: %s' % path)
+
+
+if __name__=="__main__":
+
+
+	mypath = "C:\\Users\\Zahidul\\Videos\\"
+
+
+	app=QApplication(sys.argv)
+	app.setStyle("Fusion")
+	palette = QPalette()
+	palette.setColor(QPalette.Window, QColor(53, 53, 53))
+	palette.setColor(QPalette.WindowText, QtCore.Qt.white)
+	palette.setColor(QPalette.Base, QColor(25, 25, 25))
+	palette.setColor(QPalette.AlternateBase, QColor(53, 53, 53))
+	palette.setColor(QPalette.ToolTipBase, QtCore.Qt.black)
+	palette.setColor(QPalette.ToolTipText, QtCore.Qt.white)
+	palette.setColor(QPalette.Text, QtCore.Qt.white)
+	palette.setColor(QPalette.Button, QColor(53, 53, 53))
+	palette.setColor(QPalette.ButtonText, QtCore.Qt.white)
+	palette.setColor(QPalette.BrightText, QtCore.Qt.red)
+	palette.setColor(QPalette.Link, QColor(42, 130, 218))
+	palette.setColor(QPalette.Highlight, QColor(0,0,0,0))
+	palette.setColor(QPalette.HighlightedText, QtCore.Qt.white)
+	app.setPalette(palette)
+
+	mainWindow = MainWindow()
+
+
+	sys.exit(app.exec_())
